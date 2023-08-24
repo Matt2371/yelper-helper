3fbe1b5000c4bd85cd57e7af5f1c131233dd651b
Matt2371 <mtwchen@ucdavis.edu>
2023-08-23 17:55:20 -0700

    Finished tokenizing for lstm
    Added additional unit tests

--- src/data_processing/word_tokenizer.py
+++ src/data_processing/word_tokenizer.py
@@ -1,9 +1,18 @@
 #### IMPORT PACKAGES ####
 import re
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from gensim.models import KeyedVectors
+import gensim.downloader as api
+from tqdm import tqdm
 
 #### FUNCTIONS TO TOKENIZE WORDS ####
 def basic_tokenizer(input):
-    """Tokenize input string into list of words. Ignores punctuation (,.!?)"""
+    """Tokenize input string (after converting to lowercase) into list of words. Ignores punctuation (,.!?)"""
     # lowercase input string
     input = input.lower()
     # tokenize
@@ -11,3 +20,60 @@ def basic_tokenizer(input):
     tokens = [re.sub("[.?,!]", "", word) for word in tokens]
 
     return tokens
+
+def to_word2vec(token, model, unk=0):
+    """
+    Convert token to word2vec embedding. If 
+    Params: 
+    token -- str, the input token
+    model -- gensim word2vec model (dimension 300)
+    unk -- special value for unknown words
+    Return:
+    output -- 1darray (300,), word2vec embedding
+    """
+    try:
+        output = model[token]
+    except:
+        output = unk * np.ones(300)
+
+    return output
+
+def list_embedding(input, model):
+    """
+    Get embeddings for a list of tokens
+    Params: 
+    input -- list of tokens
+    model -- gensim word2vec model (dimension 300)
+    Return:
+    output -- PyTorch tensor of shape (timesteps (review length), 300)
+    """
+
+    embeddings = []
+    for token in input:
+        # get embedding
+        embedding = to_word2vec(token, model).reshape(1, -1)
+        # convert embedding to torch tensor
+        embedding = torch.tensor(embedding, dtype=torch.float)
+        embeddings.append(embedding)
+    
+    output = torch.cat(embeddings, dim=0) # (review length, 300)
+    return output
+
+def batch_embedding(batch_input, model, pad_value=0):
+    """
+    Get embeddings for batch of reviews. Pads reviews so that each is of same length
+    Params:
+    batch_input -- list of reviews where each review is a list of tokens
+    model -- gensim word2vec model (dimension 300)
+    pad_value -- value to pad review sequences with
+    Return:
+    output -- PyTorch tensor of shape (#reviews, max review length, 300)
+    """
+    embedding_tensors = [] # list of embedding tensors for each review
+    for review in tqdm(batch_input, desc='Fetching review embeddings: '):
+        embedding_tensors.append(list_embedding(review, model))
+    
+    # pad review lengths and combine reviews into pytorch tensor
+    output = pad_sequence(embedding_tensors, batch_first=True, padding_value=pad_value)
+
+    return output # (# of reviews, max review length, 300)
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,18 +1,37 @@
 import unittest
+import numpy as np
+from src.data_processing.word_tokenizer import *
+from gensim.models import KeyedVectors
 
-from src.data_processing.word_tokenizer import basic_tokenizer
-class test_basic_tokenizer(unittest.TestCase):
-    """Test basic word tokenizer (ignores punctuation)"""
-    def test_punctuation(self):
+class test_word_tokenizer(unittest.TestCase):    
+    """Test word_tokenizer.py"""
+    def test_punctuation_basic_tokenizer(self):
+        '''Test if punctuations such as ?., will be removed'''
         input = "I like this?? Wow."
         expected = ["i", "like", "this", "wow"]
         self.assertEqual(basic_tokenizer(input), expected)
     
-    def test_apost(self):
+    def test_apost_basic_tokenizer(self):
+        '''Test if contractions are still one word'''
         input = "I don't like"
         expected = ["i", "don't", "like"]
         self.assertEqual(basic_tokenizer(input), expected)
     
+    def test_unk_to_word2vec(self):
+        '''Test if unkown word will return dim 300 vector of zeros'''
+        model = KeyedVectors.load('word2vec/word2vec-google-news-300.model')
+        input = "wooord"
+        expected = np.zeros(300)
+        np.testing.assert_almost_equal(to_word2vec(input, model), expected)
+
+    def test_shape_batch_embedding(self):
+        '''Test the shape of batch embedding'''
+        model = KeyedVectors.load('word2vec/word2vec-google-news-300.model')
+        input = [['hot', 'dog'], ['my', 'cat','disappeared'], ['this','chick','is','spicy']]
+        expected = (3,4,300)
+        self.assertEqual(tuple(batch_embedding(input, model, pad_value=0).shape), expected)
 
 if __name__ == "__main__":
     unittest.main()
+
+
